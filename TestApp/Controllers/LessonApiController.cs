00b8badd0925f40f273403a42ceadd7caf302e73
AndrewSotnikovEng <andrew.sotnikov.eng@gmail.com>
2021-09-01 20:28:52 +0300

    Added lesson API

---
+++ TestApp/Controllers/LessonApiController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http.Cors;
+using TestApp.Models;
+
+namespace TestApp.Controllers
+{
+    [EnableCors(origins: "http://localhost:5000", headers: "*", methods: "*")]
+    [Route("[controller]")]
+    [ApiController]
+    public class LessonApiController : ControllerBase
+    {
+        private AppDbContext _dbContext;
+        public LessonApiController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<object> GetLessons()
+        {
+            List<object> lessons = new List<object>();
+            foreach (var item in _dbContext.Lessons)
+            {
+                Student student = _dbContext.Students.Where(s => s.Id == item.StudentId).FirstOrDefault();
+                string date = $"{item.StartDate.ToString("dd_MM_yyyy")}";
+
+                EventDto lesson = new EventDto(student.FirstName + " " + student.LastName, date);
+                
+                lessons.Add(lesson);
+            }
+
+            return lessons;
+        }
+    }
+}
---
+++ TestApp/Models/EventDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApp.Models
+{
+    public class EventDto
+    {
+        public string Name { get; set; }
+        public string Date { get; set; }
+
+        public EventDto(string name, string date)
+        {
+            Name = name;
+            Date = date;
+        }
+    }
+}
